6c4d5738f68fb1f1c972fe6e26d9a08c6ff3e6ce
Matthew Doig <matthew.doig@bti.tv>
2014-03-31 15:22:52 -0500

    Added icons to project

---
+++ SimplePlayer/OpenSceneRenderer.h
@@ -0,0 +1,13 @@
+//
+//  OpenSceneRenderer.h
+//  SimplePlayer
+//
+//  Created by Matthew Doig on 3/31/14.
+//  Copyright (c) 2014 BTI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OpenSceneRenderer : NSObject
+
+@end
---
+++ SimplePlayer/OpenSceneRenderer.m
@@ -0,0 +1,13 @@
+//
+//  OpenSceneRenderer.m
+//  SimplePlayer
+//
+//  Created by Matthew Doig on 3/31/14.
+//  Copyright (c) 2014 BTI. All rights reserved.
+//
+
+#import "OpenSceneRenderer.h"
+
+@implementation OpenSceneRenderer
+
+@end
